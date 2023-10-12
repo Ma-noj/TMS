9d530557f89a8054ff694fc765b8c3c84beba17c
Jayashree1499 <jayasn14@gmail.com>
2023-10-12 15:38:02 +0530

    User dto created

--- src/main/java/com/ty/tms/dto/User.java
+++ src/main/java/com/ty/tms/dto/User.java
@@ -1,5 +1,24 @@
 package com.ty.tms.dto;
 
-public class User {
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
+public class User {
+	private int id;
+	private String name;
+	private String email;
+	private String password;
+	private String role;
+	public User(int id, String name, String email, String password, String role) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.password = password;
+		this.role = role;
+	}
+	
+	
 }
