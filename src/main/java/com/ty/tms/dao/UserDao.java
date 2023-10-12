b061900f3d86de141f889053d13ed27e01daa335
Jayashree1499 <jayasn14@gmail.com>
2023-10-12 15:51:29 +0530

    changes done in user dto

--- src/main/java/com/ty/tms/dao/UserDao.java
+++ src/main/java/com/ty/tms/dao/UserDao.java
@@ -1,5 +1,132 @@
 package com.ty.tms.dao;
 
-public class UserDao {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
+import com.tsm.dto.User;
+
+import connectionpool.ConnectionPool;
+
+public class UserDao {
+	
+	public User saveUser(User user) { 
+		try {
+			Class.forName("org.postresql.Driver");
+			Connection connection=ConnectionPool.getConnectionObject();
+			PreparedStatement preparedStatement=connection.prepareStatement("insert into user1 values(?,?,?,?,?)");
+			preparedStatement.setInt(1, user.getId());
+			preparedStatement.setString(2, user.getName());
+			preparedStatement.setString(3, user.getEmail());
+			preparedStatement.setString(4, user.getPassword());
+			preparedStatement.setString(5, user.getRole());
+			
+			preparedStatement.execute();
+			ConnectionPool.receiveConnectionObject(connection);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return user;
+	}
+	
+	
+	
+	public User findUserById(int id) {
+		ResultSet resultSet=null;
+		try {
+			Class.forName("org.postresql.Driver");
+			Connection connection=ConnectionPool.getConnectionObject();
+			PreparedStatement preparedStatement=connection.prepareStatement("select * from user1 where id=?");
+			preparedStatement.setInt(1, id);
+			resultSet=preparedStatement.executeQuery();
+			ConnectionPool.receiveConnectionObject(connection);
+			if(resultSet.next()) {
+				return new User(resultSet.getInt(1),resultSet.getString(2),resultSet.getString(3),resultSet.getString(4),resultSet.getString(5));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+		
+	}
+	
+	
+	
+	public User findUserByEmailPassWord(String email,String password) {
+		ResultSet resultSet=null;
+		try {
+			Class.forName("org.postresql.Driver");
+			Connection connection=ConnectionPool.getConnectionObject();
+			PreparedStatement preparedStatement=connection.prepareStatement("select * from user1 where email=? and password=?");
+			preparedStatement.setString(1, email);
+			preparedStatement.setString(2, password);
+			resultSet=preparedStatement.executeQuery();
+			ConnectionPool.receiveConnectionObject(connection);
+			if(resultSet.next()) {
+				return new User(resultSet.getInt(1),resultSet.getString(2),resultSet.getString(3),resultSet.getString(4),resultSet.getString(5));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+		
+	}
+	
+	public List<User> fetchAllUser() {
+		ResultSet resultSet=null;
+		List<User> userList=new ArrayList<>();
+		try {
+			Class.forName("org.postresql.Driver");
+			Connection connection=ConnectionPool.getConnectionObject();
+			PreparedStatement preparedStatement=connection.prepareStatement("select * from user1");
+			resultSet=preparedStatement.executeQuery();
+			ConnectionPool.receiveConnectionObject(connection);
+			if(resultSet.next()) {
+				userList.add(new User(resultSet.getInt(1),resultSet.getString(2),resultSet.getString(3),resultSet.getString(4),resultSet.getString(5)));
+				}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return userList;
+		
+	}
+	
+	
+	
+	public User updateUser(User user) {
+		try {
+			Class.forName("org.postresql.Driver");
+			Connection connection=ConnectionPool.getConnectionObject();
+			PreparedStatement preparedStatement=connection.prepareStatement("update user1 set name=?,email=?,password=? where id=?");
+			preparedStatement.setString(1, user.getName());
+			preparedStatement.setString(2, user.getEmail());
+			preparedStatement.setString(3, user.getPassword());
+			preparedStatement.setInt(4, user.getId());
+			preparedStatement.executeUpdate();
+			ConnectionPool.receiveConnectionObject(connection);
+			return user;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+		
+	}
+	
+	public boolean deleteUser(int id) {
+		try {
+			Class.forName("org.postresql.Driver");
+			Connection connection=ConnectionPool.getConnectionObject();
+			PreparedStatement preparedStatement=connection.prepareStatement("delete from user1 where id=?");
+			preparedStatement.setInt(1, id);
+			preparedStatement.executeUpdate();
+			ConnectionPool.receiveConnectionObject(connection);
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
 }
--- src/main/java/com/ty/tms/dto/User.java
+++ src/main/java/com/ty/tms/dto/User.java
@@ -1,24 +1,21 @@
 package com.ty.tms.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
 	private int id;
 	private String name;
 	private String email;
 	private String password;
 	private String role;
-	public User(int id, String name, String email, String password, String role) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.email = email;
-		this.password = password;
-		this.role = role;
-	}
+	
 	
 	
 }
